b75cdb2db985026fe262b8ca18e4d0e704ae207d
kirser5 <70627247+kirser5@users.noreply.github.com>
2020-09-29 17:02:27 +0300

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,30 @@
+package ru.mirea.pr5;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static int recursion() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if (n == 1) {
+            int m = in.nextInt();
+            if (m == 1) {
+                return recursion() + n + m;
+            } else {
+                int k = in.nextInt();
+                if (k == 1) {
+                    return recursion() + n + m + k;
+                } else {
+                    return n + m + k;
+                }
+            }
+        } else {
+            int m = in.nextInt();
+            if (m == 1) {
+                return recursion() + n + m;
+            } else {
+                return n + m;
+            }
+        }
+    }
+}
---
+++ main.java
@@ -0,0 +1,9 @@
+package ru.mirea.pr5;
+
+import static ru.mirea.pr5.Solution.recursion;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(recursion());
+    }
+}
